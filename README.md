97bb28587ad4c34ef5b3a66ef37f56352b9e06b0
turtle0x1 <dhope0000@gmail.com>
2021-10-03 02:45:25 +0100

    Add initial code to generate template from sample & start on the README

---
+++ README.md
@@ -0,0 +1,73 @@
+# LXD Metrics Zabbix Template
+
+Right now this is early stages and might have bugs, tested on zabbix 5.4.5.
+
+## Macros
+
+| Variable | Description | Must Be Set Per Host | Default |
+| -------  | ----------- | -------------------  | ------- |
+| {$HOST}  | IP Address / DNS to connect to | Yes | -     |
+| {$LXD_METRICS_CERT_PEM} | The .crt file to use to connect to LXD | Yes | - |
+| {$LXD_METRICS_KEY_PEM} | The .key file to use to connect to LXD | Yes | - |
+| {$PORT} | The port metrics are available for LXD | No | 8443 |
+| {$URL} | The url where metrics can be found | No | /1.0/metrics |
+| {$SCHEME} | The HTTP scheme to fetch data over | No | https:// |
+
+## Setup
+
+### 1. Zabbix Server Configuration
+Edit your zabbix server conf (ubuntu: `/etc/zabbix/zabbix_server.conf`) and make
+sure the following variables are uncommented and set to a location.
+
+**Remeber this location, you'll need it later**
+
+`SSLCertLocation=/home/zabbix`
+`SSLKeyLocation=/home/zabbix`
+
+Then restart zabbix `service zabbix-server restart`
+
+### 2. LXD Certificates
+
+Follow this guide to generate an authorized certificate and private key for
+zabbix to connect to the LXD instance to gather metrics.
+
+> If you do this once per host you will need to use better file names than
+> in the attached guide as they will clash when you add them to the zabbix
+> server
+
+https://linuxcontainers.org/lxd/docs/master/metrics
+
+### 3. Copy LXD certificate and private key to zabbix server
+
+You'll then need to copy the outputed `metrics.crt` & `metrics.key` to your
+zabbix server and place them under the path we set in "Step 1".
+
+### 4. Import the template into Zabbix
+
+Then download the `lxd-metrics-template.json` from this repository and import
+it into Zabbix.
+
+> Configuration -> Templates -> Create Template (very top right of the screen)
+
+### 5. Attach the template to the host
+
+*TODO explain creating a host*
+
+Attach the  template to the host
+
+> Configuration -> Hosts -> find & click your host -> Templates
+
+### 6. Setup the macros on the host
+
+As mentioned above you'll need to set some macros for the host;
+
+> > Configuration -> Hosts -> find & click your host -> Macros
+
+| Variable | Desired Value |
+| -------- | ------------- |
+| HOST     | IP Address / DNS Name (no HTTP scheme or URL)
+| LXD_METRICS_CERT_PEM | The name of the `.crt` file above (not full path, just file name) |
+| LXD_METRICS_KEY_PEM  | The name of the `.key` file above (not full path, just file name) |
+
+### 7. Wait and view your data
+Updates every 60 seconds, so give it 5 minutes and see what you can see.
---
+++ build_template/updateTemplate.php
@@ -0,0 +1,143 @@
+<?php
+
+$lines = file_get_contents(__DIR__ . "/metric_sample.txt");
+$lines = explode("\n", $lines);
+
+$currentType = "";
+
+$typeKeys = [];
+
+// Parse the metric sample for unique keys foreach TYPE
+foreach ($lines as $line) {
+    if (strpos($line, '# HELP') === 0) {
+        continue;
+    }
+    if (strpos($line, '# TYPE') === 0) {
+        $currentType = str_replace("# TYPE", "", $line);
+        $currentType = str_replace("counter", "", $currentType);
+        $currentType = str_replace("gauge", "", $currentType);
+        $currentType = trim($currentType);
+        $currentType = rtrim($currentType);
+        $typeKeys[$currentType] = [];
+        continue;
+    }
+    // Get the paramaters ready to be treated like a HTTP query string
+    $x = str_replace($currentType, "", $line);
+    $x = explode(" ", $x)[0];
+    $x = str_replace("{", "", $x);
+    $x = str_replace("}", "", $x);
+    $x = str_replace(",", "&", $x);
+    $x = str_replace("\"", "", $x);
+    // Parse paramaters as HTTP query string
+    $o = [];
+    parse_str($x, $o);
+    // Store all the keys seen for this type
+    $typeKeys[$currentType] = array_merge(array_keys($o), $typeKeys[$currentType]);
+}
+
+$zabbixItems = [];
+
+// Replace last occurnce of string https://stackoverflow.com/a/3835653/4008082
+function str_lreplace($search, $replace, $subject)
+{
+    $pos = strrpos($subject, $search);
+
+    if ($pos !== false) {
+        $subject = substr_replace($subject, $replace, $pos, strlen($search));
+    }
+
+    return $subject;
+}
+// The LLD macros we need to export
+$allSeenKeys = [];
+
+$zabbixItemUniqueIds = json_decode(file_get_contents(__DIR__ . "/uniqueIds.json"), true);
+
+foreach ($typeKeys as $type => $keys) {
+    $uniqueKeys = array_unique($keys);
+
+    $allSeenKeys = array_merge($allSeenKeys, $uniqueKeys);
+
+    usort($uniqueKeys, function ($a, $b) {
+        // Get some consistent ordering going on
+        $typeWeights = [
+            "project"=>1,
+            "name"=>2,
+            "type"=>3,
+            "device"=>4,
+            "cpu"=>5,
+            "mode"=>6
+        ];
+
+        $aWeight = isset($typeWeights[$a]) ? $typeWeights[$a] : 999999999;
+        $bWeight = isset($typeWeights[$b]) ? $typeWeights[$b] : 999999999;
+
+        return $aWeight > $bWeight;
+    });
+
+    $zabbixItemName = "$type ";
+    $zabbixItemKey = $type . '[';
+    $zabbixPreProcessPattern = '{#METRIC}' . '{';
+
+    foreach ($uniqueKeys as $key) {
+        $zabbixPreProcessPattern .= $key . '="';
+        $key = strtoupper($key);
+        $zabbixPreProcessPattern .= '{#' . $key . '}",';
+        $zabbixItemName .= "{#$key} ";
+        $zabbixItemKey .= "{#$key} ";
+    }
+
+    $zabbixItemKey = rtrim($zabbixItemKey);
+    $zabbixPreProcessPattern = str_lreplace(",", "", $zabbixPreProcessPattern);
+    $zabbixPreProcessPattern = rtrim($zabbixPreProcessPattern);
+
+    $zabbixItemKey .= "]";
+    $zabbixPreProcessPattern .= "}";
+
+    $uniqueId = isset($zabbixItemUniqueIds[$zabbixItemKey]) ? $zabbixItemUniqueIds[$zabbixItemKey] : uniqid();
+
+    $zabbixItemUniqueIds[$zabbixItemKey] = $uniqueId;
+
+    $zabbixItems[] = [
+        "name"=>$zabbixItemName,
+        "type"=>"DEPENDENT",
+        "key" => $zabbixItemKey,
+        "delay"=>'0',
+        "uuid"=>$uniqueId,
+        "preprocessing"=>[
+            [
+                "type"=>"PROMETHEUS_PATTERN",
+                "parameters"=>[
+                    $zabbixPreProcessPattern,
+                    ""
+                ]
+            ]
+        ],
+        "master_item"=>[
+          "key"=>"metrics"
+        ]
+    ];
+}
+
+$uniqueKeys = array_unique($allSeenKeys);
+
+$lldMacroPaths = [
+    [
+        "lld_macro"=>"{#METRIC}",
+        "path"=>'$[\'name\']',
+    ]
+];
+
+foreach ($uniqueKeys as $key) {
+    $lldMacroPaths[] = [
+        "lld_macro"=>'{#' . strtoupper($key) . '}',
+        "path"=>'$.labels[\'' . strtolower($key) . '\']',
+    ];
+}
+
+$template = json_decode(file_get_contents(__DIR__ . "/zbx_base_template.json"), true);
+$template["zabbix_export"]["templates"][0]["discovery_rules"][0]["item_prototypes"] = $zabbixItems;
+$template["zabbix_export"]["templates"][0]["discovery_rules"][0]["lld_macro_paths"] = $lldMacroPaths;
+
+file_put_contents(__DIR__ . "/uniqueIds.json", json_encode($zabbixItemUniqueIds, JSON_PRETTY_PRINT));
+file_put_contents(__DIR__ . "/../lxd-metrics-template.json", json_encode($template, JSON_PRETTY_PRINT));
